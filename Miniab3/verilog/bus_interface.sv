4fdbb21ca8fed7ef376789d4ed03a10b942922dc
Henry Wysong-Grass <hwysonggrass@wisc.edu>
2026-02-12 19:33:56 -0600

    reorganize minilab3

---
+++ Miniab3/verilog/bus_interface.sv
@@ -0,0 +1,7 @@
+module bus_interface(
+    
+);
+
+
+
+endmodule
---
+++ Miniab3/verilog/spart.sv
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company:         UW
+// Engineer:        Henry Wysong-Grass
+// 
+// Create Date:     2026-02-12
+// Design Name:     SPART (Special Purpose Asynchronous Receiver/Transmitter)
+// Module Name:     spart 
+//
+// Revision: 
+// Revision 1.00 - Dummy implementation
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module spart(
+    input clk,
+    input rst,
+    input iocs,
+    input iorw,
+    output rda,
+    output tbr,
+    input [1:0] ioaddr,
+    inout [7:0] databus,
+    output txd,
+    input rxd
+    );
+
+
+
+
+
+endmodule
